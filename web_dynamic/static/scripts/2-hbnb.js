bd00d8ca8806d1b5b67f5d71bdbf986fd88b47c8
Mally13 <mallymutuku13@gmail.com>
2024-02-11 18:38:08 +0300

    Task 2 and 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -11,4 +11,18 @@ $(document).ready(function() {
         }
         $("DIV.amenities H4").html(selectedAmenities.join(', '));
     });
+    $.ajax({
+        url: 'http://0.0.0.0:5001/api/v1/status/',
+        type: 'GET',
+        success: function(response) {
+            if (response.status === 'OK') {
+                $('#api_status').addClass('available');
+            } else {
+                $('#api_status').removeClass('available');
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error('Error:', error);
+        }
+    });
 });
